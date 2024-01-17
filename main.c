ee45f438f1d63012de2884c4bc4fe5508e57cad5
Shenouda Mikhael <Shenouda.Mikhael@gmail.com>
2024-01-17 19:14:29 +0200

    ignore comment

--- main.c
+++ main.c
@@ -70,8 +70,8 @@ int main(int argc, char const **argv)
 	{
 
 		n++;
-		tok = strtok(line, "\n\t\r ");
-		if (tok != NULL)
+		tok = strtok(line, "\n\t\r " );
+		if (tok != NULL && tok[0] != '#')
 		{
 
 			do_op(&stack, tok, n);
