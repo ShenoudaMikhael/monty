53a1829cc6162a5fe57bc88da87e19912df9b0e8
Shenouda Mikhael <Shenouda.Mikhael@gmail.com>
2024-01-18 04:52:59 +0200

    add

--- _add.c
+++ _add.c
@@ -56,7 +56,7 @@ void _add(stack_t **head, unsigned int n)
 
     if (*head == NULL || (*head)->next == NULL)
     {
-        dprintf(STDERR_FILENO, "L%d:  can't add, stack too short\n", n);
+        dprintf(STDERR_FILENO, "L%d: can't add, stack too short\n", n);
         exit(EXIT_FAILURE);
     }
     zero = get_dnodeint_at_index(*head, 0);
