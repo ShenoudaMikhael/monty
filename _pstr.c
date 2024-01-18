f43a509f6e05e8034148114efe7eecdb81548a6a
Salsabil <salsabeelahmed323@gmail.com>
2024-01-18 17:01:26 +0200

    try1

--- _pstr.c
+++ _pstr.c
@@ -8,10 +8,19 @@
 void _pstr(stack_t **head, unsigned int n)
 {
     (void)n;
-    while ((*head) != NULL && (*head)->n != 0 && !((*head)->n < 0 || (*head)->n > 127))
+    if (head == NULL)
     {
-        printf("%c", (*head)->n);
-        (*head) = (*head)->next;
+        printf("\n");
+    }
+    while (*head != NULL)
+    {
+        if ((*head) == NULL || (*head)->n == 0 || ((*head)->n < 0 || (*head)->n > 127))
+            break;
+        else
+        {
+            printf("%c", (*head)->n);
+            (*head) = (*head)->next;
+        }
     }
     printf("\n");
 }
